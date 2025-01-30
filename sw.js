6465fd353f4029582862972591041d2d3b40606f
Zaywop06 <241271002@alumnos.utn.edu.mx>
2025-01-30 17:49:42 -0600

    Revertí cambios

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "4.00";
+const VERSION = "5.00";
 
 /**
  * Nombre de la carpeta de caché.
